116ba787933af5b759d1de20e4193cef14277750
zuxriddin99 <zux.828@gmail.com>
2023-08-19 13:02:58 +0500

    update nginx

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -131,7 +131,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
 
